b98b5bc873f075ade27cd06f546a37f12efa9d8c
luke <luke@laptop.dev.btg>
2015-02-22 17:50:30 +0000

    Added pyopenssl to requirements

--- microauth/config.py
+++ microauth/config.py
@@ -2,11 +2,14 @@ import os
 if not 'MICROAUTH_DATABASE' in os.environ:
 	print 'You need to export a path for MICROAUTH_DATABASE'
 	print 'Eg: export MICROAUTH_DATABASE = "sqlite://///home/you/.microauth.db"'
-	raise SystemExit
+	SQLALCHEMY_DATABASE_URI = (
+    	"sqlite:///:memory:"
+	)
+else:
+	SQLALCHEMY_DATABASE_URI = (
+    	os.environ['MICROAUTH_DATABASE']
+	)
 
-SQLALCHEMY_DATABASE_URI = (
-    os.environ['MICROAUTH_DATABASE']
-)
 MASTER_KEY = None
 MASTER_KEY_NAME = "Master"
 PERMIT_NEW = True
--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ setup(
         "Flask-SQLAlchemy",
         "bcrypt",
         "requests",
+        "pyopenssl",
     ]
 )
